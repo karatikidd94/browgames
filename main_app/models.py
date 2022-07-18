e0646ad05ee85e9141fef2de10d5fb819569b92c
Jacobbshapiro <jacobbshapiro7@gmail.com>
2022-07-18 11:06:34 -0700

    adds models and index view and new home page url

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Game(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=1000)
+    link = models.CharField(max_length=1000)
+    creator = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
