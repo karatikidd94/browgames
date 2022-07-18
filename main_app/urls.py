862d25f3ec987a00a511fc744ea960e0cbea4810
lamekammi <kameronnb@gmail.com>
2022-07-18 11:51:05 -0700

    game detail page, url, and view.py

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('games/', views.games_index, name='index'),
+    path('games/<int:game_id>/', views.games_detail, name="detail"),
 ]
