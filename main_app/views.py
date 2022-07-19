44df438b4af88dab40b0920a015fd07c1bec7155
Jacobbshapiro <jacobbshapiro7@gmail.com>
2022-07-19 11:10:27 -0700

    adds .env, photo model and views and url

--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView
-from . models import Game, Genre
+from . models import Game, Genre, Photo
 from .forms import CommentForm
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+import os
+import uuid
+import boto3
 
 
 # Create your views here.
@@ -77,6 +80,22 @@ class GenreCreate(LoginRequiredMixin, CreateView):
     model = Genre
     fields = '__all__'
     success_url = '/genres/'
+
+@login_required
+def add_photo(request, game_id):
+  photo_file = request.FILES.get('photo-file', None)
+  if photo_file:
+    s3 = boto3.client('s3')
+    key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+    try:
+      bucket = os.environ['S3_BUCKET']
+      s3.upload_fileobj(photo_file, bucket, key)
+      url = f"{os.environ['S3_BASE_URL']}{bucket}/{key}"
+      Photo.objects.create(url=url, game_id=game_id)
+    except Exception as e:
+      print('An error occured uploading file to S3')
+      print(e)
+  return redirect('detail', game_id=game_id)
 
 def signup(request):
   error_message = ''
